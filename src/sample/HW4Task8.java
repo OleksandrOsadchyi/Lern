2c3da5420ab2c2dd53b82cf243dcddf0d0151ad0
OleksandrOsadchyi <moyaskrinya@gmail.com>
2018-10-21 20:33:52 +0300

    ADD HM4 Task8 Вывести в консоль числа 10, 20, 30, … 100.

---
+++ src/sample/HW4Task8.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class HW4Task8 {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 100; i= i + 10) {
+            System.out.println(i);
+
+        }
+    }
+}
