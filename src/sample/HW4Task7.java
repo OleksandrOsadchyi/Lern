1fd597c3457250e1a6d923223b6b233bf32b5a48
OleksandrOsadchyi <moyaskrinya@gmail.com>
2018-10-21 20:27:38 +0300

    ADD HM4 Task7 Вывести в консоль числа 20, 19, 18, … -10.

---
+++ src/sample/HW4Task7.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class HW4Task7 {
+    public static void main(String[] args) {
+        for (int i = 20; i >= -10; i--) {
+            System.out.println(i);
+
+        }
+    }
+
+}
