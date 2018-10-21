983f361d76db32fb499523d2cfe37304c8089150
OleksandrOsadchyi <moyaskrinya@gmail.com>
2018-10-21 20:24:31 +0300

    ADD HM4 Task6 Вывести в консоль числа 10, 11, 12, … 30.

---
+++ src/sample/HW4Task6.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.util.Scanner;
+
+public class HW4Task6 {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введіть число:");
+//        int n = sc.nextInt();
+          for (int i = 10; i <= 30; i++) {
+          System.out.println(i);
+
+        }
+    }
+
+}
