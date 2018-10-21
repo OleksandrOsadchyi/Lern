d9d2b534be9d819c02ad50db88a91fbd0b4f39dd
OleksandrOsadchyi <moyaskrinya@gmail.com>
2018-10-21 20:38:27 +0300

    ADD HM4 Task9 Вывести в консоль числа 300, 250, 200, … -200.

---
+++ src/sample/HW4Task9.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class HW4Task9 {
+    public static void main(String[] args) {
+        for (int i = 300; i >= -200; i= i - 50) {
+            System.out.println(i);
+
+        }
+    }
+
+}
